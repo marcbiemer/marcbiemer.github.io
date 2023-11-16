ed32cfa87a49d68574d6019178e063212c8b516e
Marc Biemer <marcbiemer@gmail.com>
2023-11-16 16:54:56 +0100

    Updated logo and back link

--- src/lib/components/navigation/Logo.svelte
+++ src/lib/components/navigation/Logo.svelte
@@ -1,18 +1,23 @@
 <script>
     export let link = '/';
+    export let free = false;
     import Rounding from "../ui-elements/Rounding.svelte";
 </script>
 
 <div class="logo-wrapper">
     <div class="logo">
-        <a href={link} class="txt-h">
+        <a href={link} class="txt-h" style={free ? "" : "background-color: var(--bg-default);"}>
             <svg width="36" height="24" viewBox="0 0 200 152" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M144 16C121.909 16 104 33.9086 104 56V136H184V56C184 33.9086 166.091 16 144 16ZM16 96L16 136H96V96C96 73.9086 78.0914 56 56 56C33.9086 56 16 73.9086 16 96Z" fill="var(--bg-emphasis)"/>
             </svg>
         </a>
-        <Rounding dir="tl" />
+        {#if !free}
+            <Rounding dir="tl" />
+        {/if}
     </div>
-    <Rounding dir="tl"/>
+    {#if !free}
+        <Rounding dir="tl" />
+    {/if}
 </div>
 
 <style>
@@ -28,7 +33,6 @@
         flex-direction: row;
     }
     .logo a {
-        background-color: var(--bg-default);
         border-bottom-right-radius: var(--lg);
         padding: var(--xxs) var(--sm) var(--xxs) var(--xxs);
         text-decoration: none;
